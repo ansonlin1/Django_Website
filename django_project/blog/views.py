8c993227ea47500257ad568dd826a6c6b80eb7e4
Anson Lin <anson.lin.work@gmail.com>
2020-09-01 21:05:42 -0500

    Database Registration and Migrations

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-	{
-		'author' : 'Anson',
-		'title' : 'Blog Post 1',
-		'content' : 'First post content',
-		'date_posted' : 'August 24, 2020'
-	},
-	{
-		'author' : 'John',
-		'title' : 'Blog Post 2',
-		'content' : 'Second post content',
-		'date_posted' : 'August 25, 2020'
-	}
-]
+from .models import Post
 
 def home(request):
 	context = {
-		'posts' : posts
+		'posts' : Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
